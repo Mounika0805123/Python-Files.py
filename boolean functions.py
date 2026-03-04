7b0e201e928869a141af769c7270d2ccffd708b7
Mounika0805123 <medamounika55@gmail.com>
2026-03-04 23:07:34 +0530

    Add files via upload

---
+++ boolean functions.py
@@ -0,0 +1,32 @@
+Python 3.13.12 (tags/v3.13.12:1cbe481, Feb  3 2026, 18:22:25) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> print(bool(a))
+True
+>>> b=10.12
+>>> print(bool(b))
+True
+>>> c="python"
+>>> print(bool(c))
+True
+>>> d=[]
+>>> print(bool(d))
+False
+>>> print(bool({}))
+False
+>>> 
+>>> 
+>>> 
+>>> ##validations##
+>>> 
+==================================== RESTART: E:/PYTHON PRGRMS/validations.py ====================================
+enter age  15
+invalid age
+>>> 
+=========================================== RESTART: E:/validations.py ===========================================
+enter age 5
+Invalid age
+enter a number 9
+valid
+>>> 
+>>> 
