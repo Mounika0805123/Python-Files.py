ab7464477316641363be99bae0d93a5bdf8cd006
Mounika0805123 <medamounika55@gmail.com>
2026-03-04 23:05:59 +0530

    Add files via upload

---
+++ playing with strings.py
@@ -0,0 +1,19 @@
+str='hello world'
+print("The single quotes usage is:",str)
+str="Hello world"
+print("The double quotes usage is:",str)
+str='''HELLO
+MY DEAR
+STUDENTS'''
+print(str)
+str2="Monty Python's Flying Circus"
+print(str2)
+name="MOUNIKA"
+age=19
+##F-STRING##
+print(f"My Name is:{name} and I'm {age} years old")
+##Formats##
+print("My Name is {} and I'm {} years old".format(name,age))
+##FORMAT SPECIFIER##
+print("My Name is {0} and I'm {1} years old".format(0==name,1==age))
+print("My Name is %s and I'm %d years old"%(name,age))
