738c9819129eabdc5e1c202d05b0e8e0fee236ff
Mounika0805123 <medamounika55@gmail.com>
2026-03-04 23:06:55 +0530

    Add files via upload

---
+++ ,,,,.py
@@ -0,0 +1,8 @@
+###STRINGS###
+name="hello"
+age=24
+print(f"My Nmae is {name} and I'm {age} years old")#f string
+print("My Name is {} and I'm {} years old".format(name,age))#format
+print("My Name is {0} and I'm {1} years old".format(name,age))#format specifier
+print("My Name is {n} and I'm {a} years old".format(n=name,a=age))
+print("My Name is %s and I'm %d years old"%(name,age))
