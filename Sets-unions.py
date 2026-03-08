13fd348e2ebe55fb87a13ca1007ed9d5d29dc912
Mounika0805123 <medamounika55@gmail.com>
2026-03-08 09:41:49 +0530

    Add files via upload

---
+++ Sets-unions.py
@@ -0,0 +1,18 @@
+#   UNION 
+a={1,2,3}
+b={4,5,6}
+print("The union is:",a.union(b))
+#   INTERSETION
+a={1,2,3}
+b={2,3,4,5}
+print("The Intersection is &",a&b)
+#   DIFFERENCE
+a={1,9,8,7}
+b={2,1,9,5}
+print("The Difference is using -:",a-b)
+print("The Difference is using -:",b-a)
+#   SYMMETRIC DIFFERENCE(^)
+a={1,9,8,7}
+b={2,1,9,5}
+print("The Difference is using ^ :",a-b)
+
